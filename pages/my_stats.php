c0a6ee7e78c41c4294dab5d704ad583ac578634a
sidimekov <dimasidim8582@gmail.com>
2024-04-15 23:15:43 +0300

    front updates

--- pages/my_stats.php
+++ pages/my_stats.php
@@ -24,97 +24,117 @@ require_once dirname(__DIR__) . "/backend/config.php";
         <div class="boxes">
             <div class="box">
                 <h3>Тест на простые визуальные сигналы</h3>
-                <p> Среднее время реакции</p>
+                <p>Среднее время реакции</p>
+                77%
                 <div class="progress-bar">
                     <div class="progress-bar-fill" style="width: 77%;"></div>
                 </div>
-                <p> Точность</p>
+                <p>Точность</p>
+                23%
                 <div class="progress-bar">
                     <div class="progress-bar-fill" style="width: 23%;"></div>
                 </div>
-                <p> Количество пропусков</p>
+                <p>Количество пропусков</p>
+                65%
                 <div class="progress-bar">
                     <div class="progress-bar-fill" style="width: 65%;"></div>
                 </div>
-                <p> Количество ошибок</p>
+                <p>Количество ошибок</p>
+                3%
                 <div class="progress-bar">
                     <div class="progress-bar-fill" style="width: 3%;"></div>
                 </div>
             </div>
             <div class="box">
                 <h3>Тест на простые звуковые сигналы</h3>
-                <p> Среднее время реакции</p>
+                <p>Среднее время реакции</p>
+                77%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 97%;"></div>
+                    <div class="progress-bar-fill" style="width: 77%;"></div>
                 </div>
-                <p> Точность</p>
+                <p>Точность</p>
+                23%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 76%;"></div>
+                    <div class="progress-bar-fill" style="width: 23%;"></div>
                 </div>
-                <p> Количество пропусков</p>
+                <p>Количество пропусков</p>
+                65%
                 <div class="progress-bar">
                     <div class="progress-bar-fill" style="width: 65%;"></div>
                 </div>
-                <p> Количество ошибок</p>
+                <p>Количество ошибок</p>
+                3%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 40%;"></div>
+                    <div class="progress-bar-fill" style="width: 3%;"></div>
                 </div>
             </div>
             <div class="box">
                 <h3>Тест на разные цвета</h3>
-                <p> Среднее время реакции</p>
+                <p>Среднее время реакции</p>
+                77%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 21%;"></div>
+                    <div class="progress-bar-fill" style="width: 77%;"></div>
                 </div>
-                <p> Точность</p>
+                <p>Точность</p>
+                23%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 30%;"></div>
+                    <div class="progress-bar-fill" style="width: 23%;"></div>
                 </div>
-                <p> Количество пропусков</p>
+                <p>Количество пропусков</p>
+                65%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 50%;"></div>
+                    <div class="progress-bar-fill" style="width: 65%;"></div>
                 </div>
-                <p> Количество ошибок</p>
+                <p>Количество ошибок</p>
+                3%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 40%;"></div>
+                    <div class="progress-bar-fill" style="width: 3%;"></div>
                 </div>
             </div>
             <div class="box">
                 <h3>Тест на скорость сложения в уме - сложный звуковой сигнал</h3>
-                <p> Среднее время реакции</p>
+                <p>Среднее время реакции</p>
+                77%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 0%;"></div>
+                    <div class="progress-bar-fill" style="width: 77%;"></div>
                 </div>
-                <p> Точность</p>
+                <p>Точность</p>
+                23%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 40%;"></div>
+                    <div class="progress-bar-fill" style="width: 23%;"></div>
                 </div>
-                <p> Количество пропусков</p>
+                <p>Количество пропусков</p>
+                65%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 70%;"></div>
+                    <div class="progress-bar-fill" style="width: 65%;"></div>
                 </div>
-                <p> Количество ошибок</p>
+                <p>Количество ошибок</p>
+                3%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 10%;"></div>
+                    <div class="progress-bar-fill" style="width: 3%;"></div>
                 </div>
             </div>
             <div class="box">
                 <h3>Тест на скорость сложения в уме - сложный визуальный сигнал</h3>
-                <p> Среднее время реакции</p>
+                <p>Среднее время реакции</p>
+                77%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 32%;"></div>
+                    <div class="progress-bar-fill" style="width: 77%;"></div>
                 </div>
-                <p> Точность</p>
+                <p>Точность</p>
+                23%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 78%;"></div>
+                    <div class="progress-bar-fill" style="width: 23%;"></div>
                 </div>
-                <p> Количество пропусков</p>
+                <p>Количество пропусков</p>
+                65%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 34%;"></div>
+                    <div class="progress-bar-fill" style="width: 65%;"></div>
                 </div>
-                <p> Количество ошибок</p>
+                <p>Количество ошибок</p>
+                3%
                 <div class="progress-bar">
-                    <div class="progress-bar-fill" style="width: 76%;"></div>
+                    <div class="progress-bar-fill" style="width: 3%;"></div>
                 </div>
             </div>
         </div>
