f249043aaf942efd5b25cae0ef737ba39e0a1571
sidimekov <dimasidim8582@gmail.com>
2024-04-17 13:10:32 +0300

    feat: add database data in config

--- backend/config.php
+++ backend/config.php
@@ -2,3 +2,10 @@
 
 // переменная для быстрого доступа к корню файлов
 define("ROOT", dirname(__DIR__));
+
+// данные для бд
+const DB_HOST = 'localhost';
+const DB_PORT = '3306';
+const DB_NAME = 'labs';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
